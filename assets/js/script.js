787c58ccb8db32914fded52d31052c261d503019
TitiaC62 <laetitia.crombet@gmail.com>
2023-12-12 12:28:42 +0100

    add JS audio and hide and show image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
 const boxes = document.querySelectorAll(".js-box")
+const today = new Date(Date.now())
+const dateNumber = today.getDate()
 
 boxes.forEach (box => {
     box.addEventListener('click', () => {
         const boxNumber = parseInt(box.textContent)
-        const today = new Date(Date.now())
-        const dateNumber = today.getDate()
-        console.log(today)
         if (boxNumber <= dateNumber){
-            console.log("ok")
+            playSong()
+            showImage(box)
         }
     })
 })
+
+ const url = "assets/audios/opening-song.mp3"
+ const song = new Audio(url)
+
+const playSong = () => {
+    song.pause()
+    song.currentTime = 0
+    song.play()
+    
+}
+
+const showImage = (boxToHide) => {
+    boxToHide.classList.add("hide")
+}
